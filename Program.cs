3ffa4d5ff92b6adafcddf237d9c5a3890525b6b9
Avery King <averyking345@gmail.com>
2024-05-27 21:26:53 -0400

    api complete

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
+using ToDoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,4 +28,41 @@ app.MapGet("api/todo", async (AppDbContext context) =>
     return Results.Ok(items);
 });
 
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDoItems.AddAsync(toDo);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
+
+app.MapPut("api/toDo/{id}", async (AppDbContext context, int id, ToDo toDo) =>
+{
+    var item = await context.ToDoItems.FirstOrDefaultAsync(i => i.Id == id);
+
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+    
+    item.Name = toDo.Name;
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("api/toDo/{id}", async (AppDbContext context, int id) =>
+{
+    var item = await context.ToDoItems.FirstOrDefaultAsync(i => i.Id == id);
+
+    if (item == null)
+    {
+        return Results.NotFound();
+    }
+
+    context.ToDoItems.Remove(item);
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 app.Run();
